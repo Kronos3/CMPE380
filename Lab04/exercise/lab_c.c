640cdb3761bbd3f54a8030c9bc2fa65a6fe27bf4
Andrei Tumbar <andreitumbar@gmail.com>
2021-02-16 19:56:59 -0500

    Implemented 4c

--- Lab04/exercise/lab_c.c
+++ Lab04/exercise/lab_c.c
@@ -11,42 +11,125 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 
 /*---------------------------------------------------------------------------
   These data structures are used to simulated a very simple linked list
 ---------------------------------------------------------------------------*/
-typedef struct Node{
-    int index;      
-    struct Node *next;        
+typedef struct Node
+{
+    int index;
+    struct Node* next;
     char string[40];
 } Node;
 
-typedef struct {
-    int nterms;      
-    Node *front;         
+typedef struct
+{
+    int nterms;
+    Node* front;
 } Wrapper;
 
 
+Wrapper* wrapper_init()
+{
+    Wrapper* self = malloc(sizeof(Wrapper));
+    self->nterms = 0;
+    self->front = NULL;
+
+    return self;
+}
 
 // Function prototypes
-void printList(Wrapper *wrapper_p);
+void printList(Wrapper* wrapper_p);
 
-int main(int argc, char *argv[]) {
-   Wrapper *wrapper_p;
-   Node *node_p;
-   Node *temp_p;
-   
-   // Wire the first node in 
-   
-   // Wire the second node in 
-   
-   // Clean up memory
-   
-   return 0;
+Node* node_init(int idx, const char* string)
+{
+    Node* self = malloc(sizeof(Node));
+    strncpy(self->string, string, sizeof(self->string) - 1);
+    self->index = idx;
+
+    return self;
+}
+
+void ListAppend(Wrapper* wrapper_p, const char* string)
+{
+    Node* iter = wrapper_p->front;
+    Node* last = NULL;
+
+    int i = 0;
+    while (iter)
+    {
+        i++;
+        last = iter;
+        iter = iter->next;
+    }
+
+    if (!last)
+    {
+        wrapper_p->front = node_init(i, string);
+    }
+    else
+    {
+        last->next = node_init(i, string);
+    }
+}
+
+void node_free(Node* self)
+{
+    free(self);
+}
+
+void wrapper_free(Wrapper* self)
+{
+    Node* iter = self->front;
+    int i = 0;
+    while (iter)
+    {
+        assert(i == iter->index);
+        Node* next = iter->next;
+        node_free(iter);
+        iter = next;
+    }
+
+    free(self);
+}
+
+int main(int argc, char* argv[])
+{
+    Wrapper* wrapper_p;
+    Node* node_p;
+    Node* temp_p;
+
+    wrapper_p = wrapper_init();
+
+    // Wire the first node in
+    printf("Printing 1 node linked list");
+    ListAppend(wrapper_p, "one");
+    printList(wrapper_p);
+
+    // Wire the second node in
+    printf("Printing 2 node linked list");
+    ListAppend(wrapper_p, "two");
+    printList(wrapper_p);
+
+    // Clean up memory
+    wrapper_free(wrapper_p);
+
+    return 0;
 }
 
 // Walk the linked list and print the contents 
-void printList(Wrapper *wrapper_p) {
+void printList(Wrapper* wrapper_p)
+{
+    int i = 0;
+    struct Node* iter = wrapper_p->front;
+    for (; iter && i < wrapper_p->nterms; iter = iter->next, i++)
+    {
+        assert(iter->index == i);
+        printf("node %d contains the string %s\n", iter->index, iter->string);
+    }
 
+    assert(i == wrapper_p->nterms);
+    assert(iter == NULL);
 }
